8c85642b5dff24dd6e02b6494ef984c52a29ffca
Alexander Rössler <mail.aroessler@gmail.com>
2015-06-17 10:26:06 +0200

    removed reset icomp

--- run.py
+++ run.py
@@ -18,7 +18,6 @@ try:
     launcher.cleanup_session()
     launcher.load_bbio_file('paralell_cape3.bbio')
     launcher.install_comp('thermistor_check.icomp')
-    launcher.install_comp('reset.icomp')
     launcher.install_comp('led_dim.icomp')
     launcher.install_comp('logic_fuse.icomp')
     launcher.start_process("configserver -n Uni-print-3D ~/Machineface")
